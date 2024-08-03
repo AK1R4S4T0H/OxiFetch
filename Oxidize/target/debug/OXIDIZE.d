ed5ce035c440d6492f1324436d2c1c4498b65c9e
AK1R4S4T0H <deavers.chadwick@gmail.com>
2024-08-03 15:19:26 -0400

    update

--- Oxidize/target/debug/OXIDIZE.d
+++ Oxidize/target/debug/OXIDIZE.d
@@ -1,1 +1,1 @@
-/home/ak1r4/Programs/RUst/NEo/OXIDIZE/target/debug/OXIDIZE: /home/ak1r4/Programs/RUst/NEo/OXIDIZE/src/main.rs
+/home/ak1r4/Desktop/Github/oxidize/Oxidize/target/debug/OXIDIZE: /home/ak1r4/Desktop/Github/oxidize/Oxidize/src/main.rs
